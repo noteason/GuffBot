64a45ccb13f1018051925b2024c264ed83e9307b
noteason <74929228+noteason@users.noreply.github.com>
2021-04-14 18:51:30 -0400

    Update keep_alive.py
    added it so you can now see how to make a bot in the flask

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask('')
 
-@app.route('/')
+
+@app.route('/running')
 def main():
     return "Project Running! | Thanks For Using PeelyBot"
+
+@app.route('/')
+def projectrunning():
+  return render_template('index.html')
+
+
+if __name__ == "__main__":
+  app.run()
 
 def run():
     app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     server = Thread(target=run)
-    server.start()        
+    server.start() 
